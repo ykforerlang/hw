5b22e1dd739349de3d34591da1612675086c1aaf
yankang <kang.yan@baoz.cn>
2017-03-16 09:30:51 +0800

    add log file

--- src/main/java/com/ykjava/service/HelloWordService.java
+++ src/main/java/com/ykjava/service/HelloWordService.java
@@ -1,5 +1,8 @@
 package com.ykjava.service;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 /**
@@ -7,11 +10,10 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class HelloWordService {
-    public HelloWordService() {
-        System.out.println("HelloWordService");
-    }
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     public String geneString() {
+        log.info("invoke geneString...");
         return "hws";
     }
 }
